f32b930f142babbe21c03878be1fe8f4306184fe
Porky623 <35199779+Porky623@users.noreply.github.com>
2020-11-21 11:09:34 -0500

    11/21/20, includes code for an unfair walk (original problem) and acyclical graph. Just started working on code for solving for the specific k+1, k+2 case using RREF.

--- simulations/acyclical 1.py
+++ simulations/acyclical 1.py
@@ -3,18 +3,25 @@ import random, time
 start = time.time()
 # SEED=0
 # random.seed(SEED)
-NUMTRIALS = 10000
-N = 20
+NUMTRIALS = 50000
+N = 4
 results = [0 for i in range(N)]
+increment = [1,2]
+p=.9
 for trial in range(NUMTRIALS):
     seen = {0}
     cur = 0
-    while len(seen) < N - 1:
-        cur = (cur + (-1) ** random.randint(0, 1) + N) % N
+    while len(seen) < N-1:
+        val = random.random()
+        if val<p:
+            cur=(cur+increment[0]) % N
+        else:
+            cur = (cur+increment[1]) % N
         seen.add(cur)
     for i in range(N):
         if i not in seen:
             results[i] += 1
-print(results)
+for r in results:
+    print(r/NUMTRIALS)
 end = time.time()
 print(end - start)
--- simulations/unfair walk.py
+++ simulations/unfair walk.py
@@ -3,18 +3,21 @@ import random, time
 start = time.time()
 # SEED=0
 # random.seed(SEED)
-NUMTRIALS = 10000
+NUMTRIALS = 50000
 N = 20
+p = 0.55
 results = [0 for i in range(N)]
 for trial in range(NUMTRIALS):
     seen = {0}
     cur = 0
     while len(seen) < N - 1:
-        cur = (cur + (-1) ** random.randint(0, 1) + N) % N
+        cur = (cur + (-1) ** (random.random() < p) + N) % N
         seen.add(cur)
     for i in range(N):
         if i not in seen:
             results[i] += 1
-print(results)
+for r in results:
+    print(r)
 end = time.time()
 print(end - start)
+    
